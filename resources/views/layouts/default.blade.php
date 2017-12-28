43e3d5589d5b9581aec9f3ef668c7414c3045680
zidna110 <1439276668@qq.com>
2017-12-28 16:25:02 +0000

    Finish layouts and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Sample')</title>
+    <title>@yield('title', 'Sample') - Laravel入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        <div class= "col-md-offset-1 col -md-10">
+          @yield('content')
+          @include('layouts._footer')
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.default')
-@section('title', '主页')
 
 @section('content')
-<h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+        你现在看到的是<a href="https://fsdub.com/books/laravel-essential-training-5.1">Laravel入门教程</a>的项目
+        </p>
+        <p>
+            一切将从这里开始
+        </p>
+        <p>
+            <a class ="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
