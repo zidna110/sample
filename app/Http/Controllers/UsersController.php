00ece54fb856bd98de83d8fcc6dc82f9709f2b45
zidna110 <1439276668@qq.com>
2018-01-03 05:35:15 +0000

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:25',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success', '欢迎，你注册成功了');
+        return redirect()->route('users.show', [$user]);
     }
 }
---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,9 @@
+@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+    @if(session()->has($msg))
+        <div class="flash-message">
+            <p class="alert alert-danger">
+                {{ session()->get($msg) }}
+            </p>
+        </div>
+    @endif
+@endforeach
